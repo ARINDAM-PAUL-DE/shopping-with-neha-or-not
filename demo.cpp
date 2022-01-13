c20dec71e8534be678b3205fa8114bf9db6fca26
Arindam Paul <76865623+ARINDAM-PAUL-DE@users.noreply.github.com>
2022-01-13 16:40:01 +0530

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int savings;
+    cout<<"enter your saving : ";
+    cin>>savings;
+    if(savings>500)
+    {
+        if(savings>10000)
+        {
+            cout<<"road trip with neha\n";
+
+        }
+        else
+        {
+            cout<<"shopping with neha\n";
+
+        }
+    
+       if(savings>2000)
+        {
+            cout<<"Rahmi\n";
+        }
+        else
+        {
+            cout<<"friends\n";
+        }
+        cout<<endl;
+        
+    }
+
+
+    return 0;
+}
+
+
+
